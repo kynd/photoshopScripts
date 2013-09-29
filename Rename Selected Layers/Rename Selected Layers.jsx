7cbbf252461c6a91b33dd34bcb9f5ae9920efe0b
kynd <mail@kynd.info>
2013-09-29 13:28:12 +0900

    added multiple shape layer color change

---
+++ Rename Selected Layers/Rename Selected Layers.jsx
@@ -0,0 +1,32 @@
+var newName = prompt("layer name", "new name");
+
+var layers = getSelectedLayers();
+for (var i = 0; i < layers.length; i ++){
+	layers[i].name = newName;
+}
+
+
+//get the list of multiple selected layers
+//http://www.nekomataya.info/nekojyarashi/wiki.cgi?photoshop%CA%A3%BF%F4%A5%BB%A5%EC%A5%AF%A5%C8
+
+function getSelectedLayers(){ 
+	var idGrp = stringIDToTypeID( "groupLayersEvent" );
+	var descGrp = new ActionDescriptor();
+	var refGrp = new ActionReference();
+	refGrp.putEnumerated(charIDToTypeID( "Lyr " ),charIDToTypeID( "Ordn" ),charIDToTypeID( "Trgt" ));
+	descGrp.putReference(charIDToTypeID( "null" ), refGrp );
+	executeAction( idGrp, descGrp, DialogModes.ALL );
+	var resultLayers=new Array();
+	for (var ix=0;ix<app.activeDocument.activeLayer.layers.length;ix++){resultLayers.push(app.activeDocument.activeLayer.layers[ix])}
+	var id8 = charIDToTypeID( "slct" );
+    var desc5 = new ActionDescriptor();
+    var id9 = charIDToTypeID( "null" );
+    var ref2 = new ActionReference();
+    var id10 = charIDToTypeID( "HstS" );
+    var id11 = charIDToTypeID( "Ordn" );
+    var id12 = charIDToTypeID( "Prvs" );  
+    ref2.putEnumerated( id10, id11, id12 );
+	desc5.putReference( id9, ref2 );
+	executeAction( id8, desc5, DialogModes.NO );
+	return resultLayers;
+}
